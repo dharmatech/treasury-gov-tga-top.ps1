e4e361b6986a2473d2428180baaf1c0f87abb80c
Eduardo Cavazos <wayo.cavazos@gmail.com>
2023-02-23 14:33:32 -0800

    $date param

--- treasury-gov-tga-top.ps1
+++ treasury-gov-tga-top.ps1
@@ -1,15 +1,24 @@
 ﻿
+Param($date = (get-previous-weekday))
+
+function get-previous-weekday ()
+{
+    $i = -1
+
+    while ($true)
+    {
+        if ('Saturday', 'Sunday' -contains (Get-Date).AddDays($i).DayOfWeek)
+        {
+            $i = $i - 1
+        }
+        else
+        {
+            return Get-Date (Get-Date).AddDays($i) -Format 'yyyy-MM-dd'
+        }
+    }    
+}
+
 $base = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/dts'
-
-# $date = '2022-09-01'
-
-# $date = '2022-12-01'
-# $date = '2022-12-15'
-# $date = '2022-12-16'
-
-# $date = '2022-12-19'
-
-$date = '2023-02-22'
 
 $result_raw = Invoke-RestMethod -Method Get -Uri ($base + '/dts_table_2?filter=record_date:eq:{0}&page[number]=1&page[size]=300' -f $date)
 
@@ -19,21 +28,6 @@ foreach ($row in $result_raw.data)
     $row.transaction_today_amt = [decimal]$row.transaction_today_amt
     $row.transaction_mtd_amt   = [decimal]$row.transaction_mtd_amt    
 }
-
-# $result_raw.data | ft *
-
-# $result_raw.data | Where-Object transaction_type -EQ Deposits    | Sort-Object transaction_today_amt -Descending | ft *
-# $result_raw.data | Where-Object transaction_type -EQ Withdrawals | Sort-Object transaction_today_amt -Descending | ft *
-# 
-# $result_raw.data | Where-Object transaction_type -EQ Deposits    | Sort-Object transaction_fytd_amt -Descending | ft *
-# $result_raw.data | Where-Object transaction_type -EQ Withdrawals | Sort-Object transaction_fytd_amt -Descending | ft *
-
-# $result_raw.data | Where-Object transaction_type -EQ Deposits    | Sort-Object transaction_today_amt -Descending | Select-Object -First 10 | ft *
-# $result_raw.data | Where-Object transaction_type -EQ Withdrawals | Sort-Object transaction_today_amt -Descending | Select-Object -First 10 | ft *
-
-# ($result_raw.data | Where-Object transaction_type -EQ Deposits    | Sort-Object transaction_today_amt -Descending | Select-Object -First 15) +
-# '' +
-# ($result_raw.data | Where-Object transaction_type -EQ Withdrawals | Sort-Object transaction_today_amt -Descending | Select-Object -First 15) | ft *
 
 $fields = @('record_date', 
     # 'transaction_type',
@@ -47,3 +41,12 @@ Write-Host 'DEPOSITS' -NoNewline
 '' +
 'WITHDRAWALS' +
 ($result_raw.data | Where-Object transaction_type -EQ Withdrawals | Sort-Object transaction_today_amt -Descending | Select-Object -First 15) | ft $fields
+
+exit
+
+# ----------------------------------------------------------------------
+# Example invocation
+
+.\treasury-gov-tga-top.ps1               # Default to previous weekday
+
+.\treasury-gov-tga-top.ps1 '2023-02-01'  # Specify date
